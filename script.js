a057b5391c48731ddc377c230cae341c9b65d3b0
priyanshukeshri-lab <keshripriyanshu70@gmail.com>
2026-04-09 10:00:42 +0530

    inputs is printing in console

--- script.js
+++ script.js
@@ -1,14 +1,11 @@
 let myData = [];
 
-let addBtn = document.getElementById("addButton");
+let addBtn = document.getElementById('addButton');
 addBtn.addEventListener("click",(p)=>{
-    // console.log("yoho");
-    // let val = document.querySelector("#mainInputText").value;  val.trim() !== ""
-    let val = document.querySelector("#mainInputText");
-     if (val) {
-        /*myData.push(val);
-        console.log("Current Array:", myData);*/
-        console.log("worked");
+     let val = document.getElementById('mainInputText').value;
+     if (val.trim() !== "") {
+        myData.push(val);
+        console.log("Current Array:", myData);
      } else{
         console.log("not work");
      }
