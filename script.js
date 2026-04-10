19f72472af889955ce359a0bef75379af24f6965
priyanshukeshri-lab <keshripriyanshu70@gmail.com>
2026-04-10 12:05:40 +0530

    pick one button working in console

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ let myData = [];
 
 let addBtn = document.getElementById('addButton');
 let mainInput = document.getElementById('mainInputText');
+let deciBtn = document.getElementById('disider');
 
 addBtn.addEventListener("click",(p)=>{
      inputarray();
@@ -12,6 +13,12 @@ mainInput.addEventListener("keydown", (event) => {
         inputarray();
     }
 });
+
+deciBtn.addEventListener("click",(r)=>{
+   console.log("\ntotal number of element you have entered %d",myData.length);
+   const randomItem = myData[Math.floor(Math.random() * myData.length)];
+   console.log("your item that we decided is %s", randomItem);
+})
 
 function inputarray(){
    let val = mainInput.value;
